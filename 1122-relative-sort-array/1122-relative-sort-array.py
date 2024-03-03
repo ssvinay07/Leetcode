14976c3180a427cf6960b3e59ff08791c69687f3
ssvinay07 <134847249+ssvinay07@users.noreply.github.com>
2024-03-03 22:42:52 +0530

    Time: 53 ms (8.72%), Space: 16.7 MB (86.35%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        a=[]
+        for i in arr2:
+            for j in range(arr1.count(i)):
+                a.append(i)
+        arr1.sort()
+        for k in arr1:
+            if k not in a:
+                for x in range(arr1.count(k)):
+                    a.append(k)
+        return a
+        
